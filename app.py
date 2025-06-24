d09a8c0dc2da96f6e7fe29949e484ea76a93da7d
Marvin Wottrich <mw@MacBook-Pro-von-Marvin.fritz.box>
2025-06-24 15:38:03 +0200

    fix: funktionierende app.py mit dynamischem Port

--- app.py
+++ app.py
@@ -46,8 +46,6 @@ def result(file_id):
 
 
 if __name__ == "__main__":
-    import os
     port = int(os.environ.get("PORT", 5000))
+    print(f"Running on port {port}")
     app.run(host="0.0.0.0", port=port)
-
-
